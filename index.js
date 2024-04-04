1721751059e22521f2962d6cc037b86ea8ac0ddf
Rohit kumar <rrohitkumar1077@gmail.com>
2024-04-04 17:43:18 +0530

    practice websocket with socket.io

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const {Server} = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+//socket.io
+io.on("connection",(socket)=>{
+ socket.on("message",(message)=>{
+    io.emit("message",message);
+ })
+    });
+app.use(express.static(path.resolve("./public")));
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html");
+});
+server.listen(3010,()=>{
+    console.log("Server is running on port 3010");
+});
